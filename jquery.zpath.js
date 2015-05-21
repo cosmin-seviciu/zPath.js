38070ef14280d5277d494dc22761f11a7c70c84b
Seviciu Cosmin Ioan <Only_cosmyn@yahoo.com>
2015-05-21 18:47:24 +0300

    more path calculations

--- jquery.zpath.js
+++ jquery.zpath.js
@@ -51,14 +51,14 @@
                 }else if($(this).is(path)){
                     clear.path($(this));
                 }else if($(this).is(rect)){
-
+                    clear.rect($(this));
                 }else if($(this).is(circle)){
                     clear.circle($(this));
                 }else if($(this).is(line)){
 //                    clear.path($(this));
                     clear.line($(this));
                 }else if($(this).is(polygon)){
-
+                    clear.polygon($(this));
                 }
             });
 
@@ -70,14 +70,18 @@
                 tools.dashClear(el,pathLength);
                 tools.clearFill(el);
             },
-            rect:function(){},
+            rect:function(el){
+                tools.dashClear(el,tools.getRectLength(el));
+            },
             circle:function(el){
                 tools.dashClear(el,tools.getCircleLength(el));
             },
             line:function(el){
                 tools.dashClear(el,tools.getLineLength(el));
             },
-            polygon:function(){}
+            polygon:function(el){
+                tools.dashClear(el,tools.getPolygonLength(el));
+            }
         }
         
         var tools = {
@@ -85,9 +89,86 @@
             
             /**
              *
+             * Used to get the length of a rect
+             *
+             * @param el is the rect element ex $('.rect')
+             * @return the length of the rect in px
+             */
+            getRectLength:function(el){
+                var w = el.attr('width');
+                var h = el.attr('height');
+                
+                return (w*2)+(h*2);
+            },
+            
+            /**
+             *
+             * Used to get the length of a Polygon
+             *
+             * @param el is the Polygon element ex $('.polygon')
+             * @return the length of the Polygon in px
+             */
+            getPolygonLength:function(el){
+                var points = el.attr('points');
+                points = points.split(" ");
+                var x1 = null, x2, y1 = null, y2 , lineLength = 0, x3, y3;
+                for(var i = 0; i < points.length; i++){
+                    var coords = points[i].split(",");
+                    if(x1 == null && y1 == null){
+                        
+                        if(/(\r\n|\n|\r)/gm.test(coords[0])){
+                            coords[0] = coords[0].replace(/(\r\n|\n|\r)/gm,"");
+                            coords[0] = coords[0].replace(/\s+/g,"");
+                        }
+                        
+                        if(/(\r\n|\n|\r)/gm.test(coords[1])){
+                            coords[0] = coords[1].replace(/(\r\n|\n|\r)/gm,"");
+                            coords[0] = coords[1].replace(/\s+/g,"");
+                        }
+                        
+                        x1 = coords[0];
+                        y1 = coords[1];
+                        x3 = coords[0];
+                        y3 = coords[1];
+                        
+                    }else{
+                        
+                        if(coords[0] != "" && coords[1] != ""){             
+                        
+                            if(/(\r\n|\n|\r)/gm.test(coords[0])){
+                                coords[0] = coords[0].replace(/(\r\n|\n|\r)/gm,"");
+                                coords[0] = coords[0].replace(/\s+/g,"");
+                            }
+
+                            if(/(\r\n|\n|\r)/gm.test(coords[1])){
+                                coords[0] = coords[1].replace(/(\r\n|\n|\r)/gm,"");
+                                coords[0] = coords[1].replace(/\s+/g,"");
+                            }
+
+                            x2 = coords[0];
+                            y2 = coords[1];
+                           
+                            lineLength += Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+                            
+                            x1 = x2;
+                            y1 = y2;
+                            if(i == points.length-2){
+                                lineLength += Math.sqrt(Math.pow((x3-x1), 2)+Math.pow((y3-y1),2));
+                            }
+                            
+                        }
+                    }
+                    
+                }
+                return lineLength;
+                
+            },
+            
+            /**
+             *
              * Used to get the length of a line
              *
-             * @param el is the line element
+             * @param el is the line element ex $('.line')
              * @return the length of the line in px
              */
             getLineLength:function(el){
