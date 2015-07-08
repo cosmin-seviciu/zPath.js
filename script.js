19a43c9af1e152001e5617dc2a0e3b723a6b547d
Seviciu Cosmin Ioan <Only_cosmyn@yahoo.com>
2015-07-08 20:35:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
    $('.svgTest').zPath();
     $('.svgTest1').zPath();
    $('.svgTest').zPath({action:'start'});
-   // var b =  $('.svgTest1').zPath('start');
+    var b =  $('.svgTest1').zPath('start');
    
 
 });
